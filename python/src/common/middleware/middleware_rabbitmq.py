c93e6293823a701438c494af33b175fccd029775
AlejoSendra1 <alejo.sendra12@gmail.com>
2026-03-28 17:07:41 -0300

    [feat] added MSQueue initialization and functions to implement headers

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,9 +6,36 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        channel = connection.channel()
+        channel.queue_declare(queue=queue_name)
+
+    def send(self, message):
+        return super().send(message)
+    
+    def close(self):
+        return super().close()
+    
+    def stop_consuming(self):
+        return super().stop_consuming()
+    
+    def start_consuming(self, on_message_callback):
+        return super().start_consuming(on_message_callback)
+        
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
     def __init__(self, host, exchange_name, routing_keys):
         pass
+
+    def send(self, message):
+        return super().send(message)    
+
+    def close(self):
+        return super().close()
+    
+    def stop_consuming(self):
+        return super().stop_consuming()
+    
+    def start_consuming(self, on_message_callback):
+        return super().start_consuming(on_message_callback)
